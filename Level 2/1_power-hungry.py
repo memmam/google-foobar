03aaee62c2ff26f68219438cef661b5da99aff25
Ellie M <mdmoriss@ucsd.edu>
2022-04-16 01:19:23 -0700

    Sols written 4/15

---
+++ Level 2/1_power-hungry.py
@@ -0,0 +1,42 @@
+"""
+Power Hungry
+==============
+
+Commander Lambda's space station is HUGE. And huge space stations take a LOT of power. Huge space stations with doomsday devices take even more power. To help meet the station's power needs, Commander Lambda has installed solar panels on the station's outer surface. But the station sits in the middle of a quasar quantum flux field, which wreaks havoc on the solar panels. You and your team of henchmen has been assigned to repair the solar panels, but you can't take them all down at once without shutting down the space station (and all those pesky life support systems!).
+
+You need to figure out which sets of panels in any given array you can take offline to repair while still maintaining the maximum amount of power output per array, and to do THAT, you'll first need to figure out what the maximum output of each array actually is. Write a function answer(xs) that takes a list of integers representing the power output levels of each panel in an array, and returns the maximum product of some non-empty subset of those numbers. So for example, if an array contained panels with power output levels of [2, -3, 1, 0, -5], then the maximum product would be found by taking the subset: xs[0] = 2, xs[1] = -3, xs[4] = -5, giving the product 2*(-3)*(-5) = 30. So answer([2,-3,1,0,-5]) will be "30".
+
+Each array of solar panels contains at least 1 and no more than 50 panels, and each panel will have a power output level whose absolute value is no greater than 1000 (some panels are malfunctioning so badly that they're draining energy, but you know a trick with the panels' wave stabilizer that lets you combine two negative-output panels to produce the positive output of the multiple of their power values). The final products may be very large, so give the answer as a string representation of the number.
+"""
+
+import math
+
+def solution(xs):
+    pos_list = []
+    neg_list = []
+    
+    for i in xs:
+        if i >= 0:
+            pos_list.append(i)
+        elif i < 0:
+            neg_list.append((i))
+    
+    neg_list = sorted(neg_list)
+    
+    if(len(pos_list) == 0 and len(neg_list) == 1):
+        return str(neg_list[0])
+    
+    if(len(neg_list) % 2):
+        neg_list = neg_list[0:-1]
+
+    comb_list = pos_list + neg_list
+    
+    result = 0
+    
+    for i in comb_list:        
+        if i != 0:
+            if result == 0:
+                result += 1
+            result *= i
+
+    return str(result)
---
+++ Level 3/2_prepare-the-bunnies-escape.py
@@ -0,0 +1,69 @@
+"""
+Prepare the Bunnies' Escape
+===========================
+
+You're awfully close to destroying the LAMBCHOP doomsday device and freeing Commander Lambda's bunny workers, but once they're free of the work duties the bunnies are going to need to escape Lambda's space station via the escape pods as quickly as possible. Unfortunately, the halls of the space station are a maze of corridors and dead ends that will be a deathtrap for the escaping bunnies. Fortunately, Commander Lambda has put you in charge of a remodeling project that will give you the opportunity to make things a little easier for the bunnies. Unfortunately (again), you can't just remove all obstacles between the bunnies and the escape pods - at most you can remove one wall per escape pod path, both to maintain structural integrity of the station and to avoid arousing Commander Lambda's suspicions. 
+
+You have maps of parts of the space station, each starting at a work area exit and ending at the door to an escape pod. The map is represented as a matrix of 0s and 1s, where 0s are passable space and 1s are impassable walls. The door out of the station is at the top left (0,0) and the door into an escape pod is at the bottom right (w-1,h-1). 
+
+Write a function solution(map) that generates the length of the shortest path from the station door to the escape pod, where you are allowed to remove one wall as part of your remodeling plans. The path length is the total number of nodes you pass through, counting both the entrance and exit nodes. The starting and ending positions are always passable (0). The map will always be solvable, though you may or may not need to remove a wall. The height and width of the map can be from 2 to 20. Moves can only be made in cardinal directions; no diagonal moves are allowed.
+"""
+
+def is_legal_move(y,x,map,visited,is_wr):
+    if y < 0 or y >= len(map):
+        return False
+    if x < 0 or x >= len(map[y]):
+        return False
+    if visited[is_wr][y][x]: return False
+    if map[y][x] == 1 and is_wr: return False
+    return True
+
+def solution(map):
+    visited = [[[False for i in map[0]] for i in map],[[False for i in map[0]] for i in map]]
+    visited[0][0][0] = True
+    visited[1][0][0] = True
+    
+    distance = [[[0 for i in map[0]] for i in map],[[0 for i in map[0]] for i in map]]
+
+    queue = []
+    queue.append((0,0,False))
+    
+    while len(queue) != 0:
+        curr = queue.pop(0)
+        
+        y = curr[0]
+        x = curr[1]
+        is_wr = curr[2]
+        
+        if y + 1 == len(map) and x + 1 == len(map[0]):
+            return distance[is_wr][y][x] + 1
+
+        #move right
+        if is_legal_move(y,x + 1,map,visited,is_wr):
+            new_is_wr = True if map[y][x + 1] == 1 else is_wr
+            queue.append((y,x + 1,new_is_wr))
+            distance[new_is_wr][y][x + 1] = distance[is_wr][curr[0]][curr[1]] + 1
+            visited[new_is_wr][y][x + 1] = 1
+
+        #move down
+        if is_legal_move(y + 1,x,map,visited,is_wr):
+            new_is_wr = True if map[y + 1][x] == 1 else is_wr
+            queue.append((y + 1,x,new_is_wr))
+            distance[new_is_wr][y + 1][x] = distance[is_wr][curr[0]][curr[1]] + 1
+            visited[new_is_wr][y + 1][x] = 1
+
+        #move left
+        if is_legal_move(y,x - 1,map,visited,is_wr):
+            new_is_wr = True if map[y][x - 1] == 1 else is_wr
+            queue.append((y,x - 1,new_is_wr))
+            distance[new_is_wr][y][x - 1] = distance[is_wr][curr[0]][curr[1]] + 1
+            visited[new_is_wr][y][x - 1] = 1
+
+        #move up
+        if is_legal_move(y - 1,x,map,visited,is_wr):
+            new_is_wr = True if map[y - 1][x] == 1 else is_wr
+            queue.append((y - 1,x,new_is_wr))
+            distance[new_is_wr][y - 1][x] = distance[is_wr][curr[0]][curr[1]] + 1
+            visited[new_is_wr][y - 1][x] = 1
+
+    return 0
