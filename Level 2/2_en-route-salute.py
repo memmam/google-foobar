e1da3817f8bf640ce68bfd6ce5cd0f77598ebc20
Ellie M <mdmoriss@ucsd.edu>
2022-07-28 13:57:08 -0700

    Update 2_en-route-salute.py

--- Level 2/2_en-route-salute.py
+++ Level 2/2_en-route-salute.py
@@ -1,7 +1,6 @@
 """
 En Route Salute
 ===============
-
 Commander Lambda loves efficiency and hates anything that wastes time. She's a busy lamb, after all! She generously rewards henchmen who identify sources of inefficiency and come up with ways to remove them. You've spotted one such source, and you think solving it will help you build the reputation you need to get promoted.
 
 Every time the Commander's employees pass each other in the hall, each of them must stop and salute each other - one at a time - before resuming their path. A salute is five seconds long, so each exchange of salutes takes a full ten seconds (Commander Lambda's salute is a bit, er, involved). You think that by removing the salute requirement, you could save several collective hours of employee time per day. But first, you need to show her how bad the problem really is.
@@ -15,19 +14,15 @@ Write a function answer(s) which takes a string representing employees walking a
 
 Test cases
 ==========
-Your code should pass the following test cases.
-Note that it may also be run against hidden test cases not shown here.
-
--- Python cases --
 Input:
 solution.solution(["1.11", "2.0.0", "1.2", "2", "0.1", "1.2.1", "1.1.1", "2.0"])
 Output:
-0.1,1.1.1,1.2,1.2.1,1.11,2,2.0,2.0.0
+    0.1,1.1.1,1.2,1.2.1,1.11,2,2.0,2.0.0
 
 Input:
 solution.solution(["1.1.2", "1.0", "1.3.3", "1.0.12", "1.0.2"])
 Output:
-1.0,1.0.2,1.0.12,1.1.2,1.3.3
+    1.0,1.0.2,1.0.12,1.1.2,1.3.3
 """
 
 def solution(s):
