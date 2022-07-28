52b72ca2e1f92a930a1f4923c20ce76adec5cdde
Ellie M <mdmoriss@ucsd.edu>
2022-07-28 13:57:05 -0700

    Test cases

--- Level 2/1_power-hungry.py
+++ Level 2/1_power-hungry.py
@@ -1,7 +1,6 @@
 """
 Power Hungry
 ==============
-
 Commander Lambda's space station is HUGE. And huge space stations take a LOT of power. Huge space stations with doomsday devices take even more power. To help meet the station's power needs, Commander Lambda has installed solar panels on the station's outer surface. But the station sits in the middle of a quasar quantum flux field, which wreaks havoc on the solar panels. You and your team of henchmen has been assigned to repair the solar panels, but you can't take them all down at once without shutting down the space station (and all those pesky life support systems!).
 
 You need to figure out which sets of panels in any given array you can take offline to repair while still maintaining the maximum amount of power output per array, and to do THAT, you'll first need to figure out what the maximum output of each array actually is. Write a function answer(xs) that takes a list of integers representing the power output levels of each panel in an array, and returns the maximum product of some non-empty subset of those numbers. So for example, if an array contained panels with power output levels of [2, -3, 1, 0, -5], then the maximum product would be found by taking the subset: xs[0] = 2, xs[1] = -3, xs[4] = -5, giving the product 2*(-3)*(-5) = 30. So answer([2,-3,1,0,-5]) will be "30".
@@ -11,17 +10,13 @@ Each array of solar panels contains at least 1 and no more than 50 panels, and e
 
 Test cases
 ==========
-Your code should pass the following test cases.
-Note that it may also be run against hidden test cases not shown here.
-
--- Python cases --
 Input:
-    solution.solution([2, 0, 2, 2, 0])
+solution.solution([2, 0, 2, 2, 0])
 Output:
     "8"
 
 Input:
-    solution.solution([-2, -3, 4, -5])
+solution.solution([-2, -3, 4, -5])
 Output:
     "60"
 """
