46efbb57b2e0a3eae5377af820c43b5e9791779a
Ellie M <mdmoriss@ucsd.edu>
2022-07-28 10:21:29 -0700

    Test cases 3-2

--- Level 3/2_prepare-the-bunnies-escape.py
+++ Level 3/2_prepare-the-bunnies-escape.py
@@ -7,6 +7,22 @@ You're awfully close to destroying the LAMBCHOP doomsday device and freeing Comm
 You have maps of parts of the space station, each starting at a work area exit and ending at the door to an escape pod. The map is represented as a matrix of 0s and 1s, where 0s are passable space and 1s are impassable walls. The door out of the station is at the top left (0,0) and the door into an escape pod is at the bottom right (w-1,h-1). 
 
 Write a function solution(map) that generates the length of the shortest path from the station door to the escape pod, where you are allowed to remove one wall as part of your remodeling plans. The path length is the total number of nodes you pass through, counting both the entrance and exit nodes. The starting and ending positions are always passable (0). The map will always be solvable, though you may or may not need to remove a wall. The height and width of the map can be from 2 to 20. Moves can only be made in cardinal directions; no diagonal moves are allowed.
+
+Test cases
+==========
+Your code should pass the following test cases.
+Note that it may also be run against hidden test cases not shown here.
+
+-- Python cases --
+Input:
+solution.solution([[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]])
+Output:
+7
+
+Input:
+solution.solution([[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]])
+Output:
+11
 """
 
 def is_legal_move(y,x,map,visited,is_wr):
